0708d3a0d8c4f3edcbb4e93e7218b5b21d5a7cc4
luizaugustoventura <luizaugustoventu1998@hotmail.com>
2020-03-26 11:28:18 -0300

    Backend da aplicação

---
+++ backend/src/database/migrations/20200324230353_create_ongs.js
@@ -0,0 +1,16 @@
+// O que vai acontecer quando eu executar essa migration
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', function(table) {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+// O que vai acontecer caso dê algum problema e eu precise desfazer algo
+exports.down = function(knex) {
+    return knex.schema.dropTable('ongs');
+};
